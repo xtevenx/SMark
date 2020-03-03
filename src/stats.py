effe0e365c5326a2f689c96c292696a8fc509301
Steven Xia <stevenxia@protonmail.com>
2020-03-03 03:06:44 -0800

    Add doc-strings to `src/stats.py`.

--- src/stats.py
+++ src/stats.py
@@ -3,12 +3,25 @@ from typing import Sequence
 
 
 def mean(data: Sequence[float]) -> float:
+    """
+    Get the arithmetic mean of a sequence of numbers.
+    :param data: the `Sequence` of which to get the mean of.
+    :return: a `float`, the mean of the sequence `data`.
+    """
+
     assert len(data) > 0
 
     return sum(data) / len(data)
 
 
 def stddev(data: Sequence[float]) -> float:
+    """
+    Get the standard deviation of a sequence of numbers.
+    :param data: the `Sequence` of which to get the standard deviation
+        of.
+    :return: a `float`, the standard deviation of the sequence `data`.
+    """
+
     assert len(data) > 0
 
     mu = mean(data)
