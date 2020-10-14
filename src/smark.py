b78eba5238df8acf1bdc77e250b30e27ee8666fd
Steven Xia <stevenxia@protonmail.com>
2020-10-13 22:13:02 -0700

    Add bounds checking to input values.

--- src/smark.py
+++ src/smark.py
@@ -1,6 +1,6 @@
 if __name__ != "__main__":
     print("Error: SMark is a standalone script.")
-    exit(0)
+    exit(1)
 
 import display
 import scale
@@ -17,15 +17,28 @@ print("Enter the individual scores below (one on each line). Enter an empty \n"
 
 inputs = []
 try:
-    while True:
-        inputs.append(float(input()) / total_score)
-except (ValueError, EOFError):
-    ...
+    while this_input := input().strip():
+        decimal_input = float(this_input) / total_score
+        assert 0 <= decimal_input <= 1
+        inputs.append(decimal_input)
+
+except ValueError:
+    ordinals = ["st", "nd", "rd"] + ["th"] * 7
+    formatted_num = f"{len(inputs) + 1}{ordinals[len(inputs) % 10]}"
+    print(f"Error: the {formatted_num} input was unable to be parsed.")
+    exit(1)
+
+except AssertionError:
+    ordinals = ["st", "nd", "rd"] + ["th"] * 7
+    formatted_num = f"{len(inputs) + 1}{ordinals[len(inputs) % 10]}"
+    print(f"Error: the {formatted_num} input is not in range (0 - {total_score}).")
+    exit(1)
 
 display.display_info(inputs, total_score, header="input data statistics")
 
 print()
-scale_mean = float(input("What is the target average (mean) percentage (0-100)> ")) / 100
+scale_mean = float(input("What is the target average (mean) percentage (0 - 100)> ")) / 100
+assert 0 <= scale_mean <= 1, "Error: the target average (mean) is not in range (0 - 100)."
 
 scale_func = scale.inverse_power_scale if scale_mean > stats.mean(inputs) else scale.power_scale
 outputs, _ = scale.scale(inputs, scale_mean, scale_func)
